48d1793dc5c9d795e9cbfce50eb61a06f24e6a53
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2022-03-31 20:42:22 -0700

    Hello, Mister Andersen

---
+++ app.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# slackbot PoC JDSmith
+"""hello there"""
+import os
+# Use the package we installed
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+
+@app.command("/hello-socket-mode")
+def hello_command(ack, body):
+    user_id = body["user_id"]
+    ack(f"You hear that <@{user_id}>?... That is the sound of inevitability.")
+
+
+@app.event("app_mention")
+def event_test(say):
+    say("Mister Anderson")
+
+
+# Listens to incoming messages that contain "hello"
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click Me"},
+                    "action_id": "button_click",
+                },
+            }
+        ],
+        text=f"Hey there <@{message['user']}>!",
+    )
+
+
+@app.action("button_click")
+def action_button_click(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}> clicked the button")
+
+
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+    try:
+        # views.publish is the method that your app uses to push a view to the Home tab
+        client.views_publish(
+            # the user that opened your app's app home
+            user_id=event["user"],
+            # the view object that appears in the app home
+            view={
+                "type": "home",
+                "callback_id": "home_view",
+
+                # body of the view
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Welcome to your _App's Home_* :tada:"
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "This button won't do much for now but you can set up a "
+                                    "listener for it using the `actions()` method and passing its "
+                                    "unique `action_id`. See an example in the `examples` folder "
+                                    "within your Bolt app. "
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {"type": "plain_text", "text": "Click me!"},
+                                "action_id": "home_button",
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
+
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
+
+@app.action("home_button")
+def action_home_button_click(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(channel="CL1PDRL15", text=f"ring ring <@{body['user']['id']}>")
+
+
+# Start your app
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
---
+++ readme.md
@@ -0,0 +1,47 @@
+##Baby's First Slackbot
+*If running locally during development, skip ssh and scp commands*
+#setup
+**SSH to matrix**
+
+ssh -i your-pem.key opc@its.public.ip
+
+**one time venv creation**
+
+python -V
+
+use the version in following command
+
+python3.6 -m venv ~/matrix
+
+**activate venv**
+
+source ~/matrix/bin/activate
+
+**get packages**
+
+pip3 install slack_bolt
+
+**on local machine**
+
+scp /Users/you/agent_smith/app.py opc@its.public.ip:~
+
+**one time setup**
+
+set environment variables for 
+
+SLACK_BOT_TOKEN, SLACK_APP_TOKEN, SLACK_SIGNING_SECRET
+
+make app.py executable
+
+**start script and safely exit ssh session without Agent Smith turning off**
+nohup ./app.py &
+exit
+
+#Future
+**SSH to matrix**
+
+ssh -i your-pem.key opc@its.public.ip
+
+**view script output, if any**
+
+nano nohup.out
